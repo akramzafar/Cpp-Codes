530c0c2337947097babf9711caeb4886eac5c166
akramzafar <89122571+akramzafar@users.noreply.github.com>
2021-12-02 16:15:35 +0500

    sum of two int
    Calculator starting

---
+++ github.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Enter first number: ";
+	int one; cin>>one;
+	cout<<"\nEnter second number: ";
+	int sec; cin>>sec;
+		cout<<"\nADDITION IS: ";
+		int s;
+		s=one+sec;
+		cout<<s;
+	return 0;
+}
